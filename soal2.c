b5363fd6796b6f22a23cbf80f94e3968eef82f00
Unknown <yolandahertita903@gmail.com>
2017-10-25 11:07:01 +0700

    cuma baru bikin masukkan nama masing-masing player

---
+++ soal2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	char nama1[100], nama2[100];
+	printf("Masukkan nama untuk player1: "); scanf("%s", nama1);
+	printf("Masukkan nama untuk player2: "); scanf("%s", nama2);
+
+		
+
+
+
+	return 0;
+}
