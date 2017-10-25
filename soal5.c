f46ec0e2018ac0d983e9775fc7e596e5c5eaf565
Unknown <yolandahertita903@gmail.com>
2017-10-25 09:53:18 +0700

    sepertinya sudah berhasil :)

--- soal5.c
+++ soal5.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int wait = 1;
+int tunggu = 1;
 pthread_t threadid[2];
 
-void* copy_file(void *arg){
+void* cari(void *arg){
 
     pthread_t id=pthread_self();
     
     if(pthread_equal(id,threadid[0])){
-        char c;
-    
-        FILE* finp=fopen("baca.txt","r");
-        FILE* fout=fopen("salin1.txt","w");
-    
-        if(finp!=NULL){
-            while(c=fgetc(finp)){
-                if(c==EOF) break;
-                fputc(c, fout);
-            }
-            wait = 0;
-            fclose(finp);
-        }
+        pid_t child_id;
+        int status;
 
-        fclose(fout);
+        child_id = fork();
+
+        if(child_id == 0) printf("Ifah = ");
+        else{
+            while((wait(&status)) > 0);
+            system("grep -o Ifah Novel.txt | wc -l");
+        }
+        tunggu = 0;
     }
     else if(pthread_equal(id,threadid[1])){
-        while(wait);
-        char c;
-    
-        FILE* finp=fopen("salin1.txt","r");
-        FILE* fout=fopen("salin2.txt","w");
-    
-        if(finp!=NULL){
-            while(c=fgetc(finp)){
-                if(c==EOF) break;
-                fputc(c, fout);
-            }
-            fclose(finp);
-        }
+        while(tunggu);
 
-        fclose(fout);
+        pid_t child_id;
+        int status;
+
+        child_id = fork();
+
+        if(child_id == 0) printf("Fina = ");
+        else{
+            while((wait(&status)) > 0);
+            system("grep -o Fina Novel.txt | wc -l");
+        }
     }
 
     return NULL;
@@ -51,13 +47,10 @@ int main(){
     int i;
     int ret1, ret2;
 
-    const char *word1 = "Ifah";
-    const char *word2 = "Fina";
-
-    ret1 = pthread_create(&(threadid[i]),NULL,cari,(void *)word1);
+    ret1 = pthread_create(&(threadid[0]),NULL,&cari,NULL);
     if(ret1) exit(EXIT_FAILURE);
 
-    ret2 = pthread_create(&(threadid[i]),NULL,cari,(void *)word2);
+    ret2 = pthread_create(&(threadid[1]),NULL,&cari,NULL);
     if(ret2) exit(EXIT_FAILURE);
 
     pthread_join(threadid[0], NULL);
