558c68b85694866541cfcca1540300f97b353af5
Unknown <dtobing81@gmail.com>
2017-10-25 13:00:25 +0700

    soal 1 untuk penjual
    fungsi untuk menambah dan melihat stok sudah ada

--- soal1_Penjual.c
+++ soal1_Penjual.c
@@ -14,17 +14,31 @@ char barang[6][20];
 void main(){
 	key_t key=7667;
 	int *jlh_stok;
-
-
+	int c;
+	
 	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
 	jlh_stok = shmat(shmid, NULL, 0);
 	while (1){
-
-
+		printf("Pilih	:\n1.Lihat Barang\n2.Tambah Stok\n");
+		scanf("%d", &c)
+		if(c==1){
+			for(int i=0; i<6 ; i++){
+				if(jlh_stok[i]>0){
+				printf("Jumlah %s di stok = %d barang\n", barang[i], jlh_stok[i]);
+				}
+			}
+		}
+		else if(c==2){
+			scanf("%s %d", n_barang, &tambah);
+			for(i=0;i<6;i++){
+				if(!strcmp(n_barang, barang[0]){
+					jlh_stok[i]+=tambah;
+				})
+			}
+		}
+		else{
+			printf("perintah salah!\n");
+		}
 
 	}
-
-
-
-
 }
