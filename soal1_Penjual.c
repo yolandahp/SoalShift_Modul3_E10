a558e9e9dfaf68327f48cb56616b8babb19adf23
Unknown <dtobing81@gmail.com>
2017-10-25 13:48:56 +0700

    Sudah Fix untuk soal 1
    silahkan di seruput

--- soal1_Penjual.c
+++ soal1_Penjual.c
@@ -1,28 +1,24 @@
-#include<cstdio>
+#include<stdio.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<unistd.h>
+#include <string.h>
 
-char barang[6][20];
-	barang[0] = "MP4A1";
-	barang[1] = "PM2-V1";
-	barang[2] = "SPR-3";
-	barang[3] = "SS2-V5";
-	barang[4] = "SPG1_V3";
-	barang[5] = "MINE"
 	
 void main(){
 	key_t key=7667;
 	int *jlh_stok;
 	int c, i, tambah;
 
+
+char barang[6][10]={"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1_V3", "MINE"};
 	
-	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+	int shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
 	jlh_stok = shmat(shmid, NULL, 0);
 	while (1){
 
 		printf("Pilih	:\n1.Lihat Barang\n2.Tambah Stok\n");
-		scanf("%d", &c)
+		scanf("%d", &c);
 		if(c==1){
 			for(int i=0; i<6 ; i++){
 				if(jlh_stok[i]>0){
@@ -31,13 +27,13 @@ void main(){
 			}
 		}
 		else if(c==2){
-			char n_barang[20];
+			char n_barang[10];
 			scanf("%s %d", n_barang, &tambah);
 			for(i=0;i<6;i++){
-				if(!strcmp(n_barang, barang[i]){
+				if(!strcmp(n_barang, barang[i])){
 					jlh_stok[i]+=tambah;
 					printf("Jumlah %s di stok = %d barang\n", barang[i], jlh_stok[i]);
-				})
+				}
 			}
 		}
 		else{
@@ -47,5 +43,4 @@ void main(){
 	}
 	shmdt(jlh_stok)	;
 	shmctl(shmid,IPC_RMID,NULL);
-	return 0;
 }
--- soal1_pembeli.c
+++ soal1_pembeli.c
@@ -1,26 +1,21 @@
-#include<cstdio>
+#include<stdio.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<unistd.h>
-
-char barang[6][20];
-	barang[0] = "MP4A1";
-	barang[1] = "PM2-V1";
-	barang[2] = "SPR-3";
-	barang[3] = "SS2-V5";
-	barang[4] = "SPG1_V3";
-	barang[5] = "MINE"
+#include<string.h>
 	
 void main(){
 	key_t key=7667;
 	int *jlh_stok;
 	int c, i, kurang;
+
+	char barang[6][10]={"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1_V3", "MINE"};
 	
-	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+	int shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
 	jlh_stok = shmat(shmid, NULL, 0);
 	while (1){
 		printf("Pilih	:\n1.Lihat Barang\n2.Beli Barang\n");
-		scanf("%d", &c)
+		scanf("%d", &c);
 		if(c==1){
 			for(int i=0; i<6 ; i++){
 				if(jlh_stok[i]>0){
@@ -29,6 +24,7 @@ void main(){
 			}
 		}
 		else if(c==2){
+			char n_barang[10];
 			scanf("%s %d", n_barang, &kurang);
 			for(i=0;i<6;i++){
 				if(!strcmp(n_barang, barang[i])){
@@ -37,7 +33,7 @@ void main(){
 					printf("Jumlah %s di stok = %d barang\n", barang[i], jlh_stok[i]);
 				}
 				else{
-					printf("Barang di stok tidak cukup");
+					printf("Barang di stok tidak cukup\n\n");
 						}
 				break;
 					}
@@ -50,5 +46,4 @@ void main(){
 	}
 	shmdt(jlh_stok)	;
 	shmctl(shmid,IPC_RMID,NULL);
-	return 0;
 }
